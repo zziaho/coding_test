6696c8d81cfc3e6b4c8824b5c412610d584ecc91
ziaho <rlawlgh1100@gmail.com>
2022-04-13 23:09:36 +0900

    정수 1개 입력받아 그대로 출력하기

---
+++ codingtest/codeup100/정수1개입력받아그대로출력하기.java
@@ -0,0 +1,16 @@
+package codeup100;
+
+public class 정수1개입력받아그대로출력하기 {
+
+	public static void main(String[] args) {
+
+//		정수형(int)으로 변수를 선언하고, 변수에 정수값을 저장한 후
+//		변수에 저장되어 있는 값을 그대로 출력해보자.
+		
+		int a; // int형 변수 a 선언
+		a = 10; // a 에 값 저장
+		System.out.println(a); // a 출력
+		
+	}
+
+}
