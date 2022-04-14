0a959fbcb786a191fe618c2e6f87233d3afb5242
ziaho <rlawlgh1100@gmail.com>
2022-04-14 17:00:01 +0900

    실수 1개 입력받아 그대로 출력하기

---
+++ codingtest/codeup100/실수1개입력받아그대로출력하기11.java
@@ -0,0 +1,16 @@
+package codeup100;
+
+public class 실수1개입력받아그대로출력하기11 {
+
+	public static void main(String[] args) {
+
+//		실수형(float)로 변수를 선언하고 그 변수에 실수값을 저장한 후
+//		저장되어 있는 실수값을 출력해보자.
+		
+		float f; // 선언
+		f = 3.14f; // 값 대입 --> float형은 숫자 끝에 f를 붙여 표시한다
+		System.out.println(f); // 값 출력
+		
+	}
+
+}
