9be3408b4c2b1f069307a2f8ca6fc92d37341f9d
ziaho <rlawlgh1100@gmail.com>
2022-04-14 17:10:00 +0900

    정수 2개 입력받아 그대로 출력하기

---
+++ codingtest/codeup100/정수2개입력받아그대로출력하기12.java
@@ -0,0 +1,16 @@
+package codeup100;
+
+public class 정수2개입력받아그대로출력하기12 {
+
+	public static void main(String[] args) {
+
+//		정수(int) 2개를 입력받아 그대로 출력해보자.
+		
+		int a, b;
+		a = 10;
+		b = 20;
+		System.out.printf("%d,%d", a,b); // printf 사용
+		
+	}
+
+}
