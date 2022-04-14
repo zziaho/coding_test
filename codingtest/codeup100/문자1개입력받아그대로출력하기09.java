65ac780e3167ffe44f21e526f9c85472ffd05e50
ziaho <rlawlgh1100@gmail.com>
2022-04-14 16:57:48 +0900

    문자 1개 입력받아 그대로 출력하기

---
+++ codingtest/codeup100/문자1개입력받아그대로출력하기09.java
@@ -0,0 +1,16 @@
+package codeup100;
+
+public class 문자1개입력받아그대로출력하기09 {
+
+	public static void main(String[] args) {
+
+//		문자형(char)으로 변수를 하나 선언하고, 변수에 문자를 저장한 후
+//		변수에 저장되어 있는 문자를 그대로 출력해보자.
+		
+		char a; // 변수 선언
+		a = 'A'; // 값 대입
+		System.out.println(a); // 값 출력
+		
+	}
+
+}
