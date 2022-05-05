f8445a6ecdc9150c581e19f8501d3a5671df1bc7
ziaho <rlawlgh1100@gmail.com>
2022-05-05 19:33:56 +0900

    Hello World!를 화면에 출력하는 문제 (예제 출력과 똑같이 출력해야 합니다.)

---
+++ codingtest/baekjoon/step1/HelloWorld.java
@@ -0,0 +1,13 @@
+package baekjoon.step1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+//		Hello World!를 화면에 출력하는 문제 (예제 출력과 똑같이 출력해야 합니다.)
+		
+		System.out.println("Hello World!");
+		
+	}
+
+}
