59a0a557cd80b13d5b071f0a4c42259b2210ffc1
ziaho <rlawlgh1100@gmail.com>
2022-05-06 00:14:21 +0900

    고양이

---
+++ codingtest/baekjoon/step1/고양이.java
@@ -0,0 +1,21 @@
+package baekjoon.step1;
+
+public class 고양이 {
+
+	public static void main(String[] args) {
+
+//		아래 예제와 같이 고양이를 출력하시오.
+		
+//		\    /\
+//		 )  ( ')
+//		(  /  )
+//		 \(__)|
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+	}
+
+}
